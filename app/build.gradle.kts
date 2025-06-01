b78983b1d8c0119cbea1e62dbdb856053a640093
Davor Georgievski <davor.georgievski@yahoo.com>
2025-06-01 19:03:11 +0200

    Commit 1: Povrzuvanje so FireBase

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android") version "1.9.23"
+    id("com.google.gms.google-services") // <--- важно
+}
+
+android {
+    namespace = "com.example.fitbuddy"
+    compileSdk = 35
+
+    buildFeatures {
+        viewBinding= true
+    }
+
+    defaultConfig {
+        applicationId = "com.example.fitbuddy"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+
+    // Firebase modules
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-messaging")
+
+    // Google Sign-In
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
+
+    // Facebook Login
+    implementation("com.facebook.android:facebook-login:16.3.0")
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
